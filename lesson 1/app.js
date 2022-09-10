c0378bc21ffd3d251d2c1af79f4793265b1dfe5c
Yanina <moontmorency@gmail.com>
2022-09-10 19:00:21 +0200

    Learn variables

--- lesson 1/app.js
+++ lesson 1/app.js
@@ -3,3 +3,21 @@ document.querySelectorAll(".btn").forEach((item) => {
     alert("this is awesome");
   });
 });
+
+document.write('hello world!')
+alert('hello people')
+console.log('hello it is me');
+// Can see in the browser console 
+
+// Could be without semicolon, used to show the end of the command
+
+
+// Variables
+let address, zip, state;
+address = '101 main street';
+console.log(address);
+
+zip = '60040';
+state = 'CA';
+console.log(address, zip, state);
+
