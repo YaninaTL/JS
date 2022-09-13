8969b80058da6afd1b1402ce07fde4e0829cb018
Yanina <moontmorency@gmail.com>
2022-09-13 21:36:41 +0200

    Learn functions

--- lesson 3/app.js
+++ lesson 3/app.js
@@ -65,6 +65,7 @@ greet(susy);
 
 
 //1 inch 2.54cm
+//RETURN
 
 
 const wallHeight = 80;
@@ -84,7 +85,29 @@ const width = calculate(100);
 const height = calculate(wallHeight);
 
 const dimensions = [width, height];
-console.log(dimensions)
+console.log(dimensions);
 //[0, 0] when width = 0, height = 0
 //after adding calculate and call the function -> 254 cm for the width and 203.2 cm for height
 
+//declaration
+function addValues(num1, num2) {
+    return num1 + num2;
+}
+
+const firstValue = addValues(3, 4);
+const secondValue = addValues(12, 34);
+
+//expression
+const add = function addValues(num1, num2) {
+    return num1 + num2;
+};
+//Can delete the name of the function
+// const thirdValue = add(5, 6);
+const values_ = [firstValue, secondValue, thirdValue];
+// Instead of thirdValue could be:
+//const values_ = [firstValue, secondValue, 11]; OR const values_ = [firstValue, secondValue, add(5, 6)];
+console.log(values_)
+//[7, 46, 11]
+
+const multiply = (num1, num2) => num1 * num2;
+//arrow function
