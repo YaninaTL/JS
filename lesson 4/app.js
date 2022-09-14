bf3dbbbec2908292d5f399d0d5ded7649c7d84ee
Yanina <moontmorency@gmail.com>
2022-09-14 21:12:19 +0200

    Learn conditions and comparisons

--- lesson 4/app.js
+++ lesson 4/app.js
@@ -24,3 +24,53 @@ console.log(person.siblings[2]);
 //bob
 person.greeting();
 //Hello, my name is PETER
+
+//Conditions
+
+const value = true;
+// const value = 2 > 1;
+//This is true
+const value2 = false;
+//const value2 = 1 > 2;
+
+if (value) {
+    console.log('Hello!');
+} else {
+    console.log('Bye!')
+}
+// Hello!
+
+
+
+const num1 = 4;
+const num2 = 6;
+
+if (num1 > num2) {
+    console.log('First number is bigger than second');
+} else {
+    console.log("Second number is bigger than first");
+}
+//If we have 2 equal numbers (6 and 6) -> "Second number is bigger than first" as num1 > num2 (like >=)
+
+
+// !
+const value_ = true;
+if (!value) {
+    console.log('value is true');
+}
+//The code won't run, it's looking for the opposite
+
+// ==  ===
+const n1 = 6;
+const n2 = '6';
+
+const value_1 = n1 == n2;
+const value_2 = n1 === n2;
+
+console.log(value_1);
+console.log(value_2);
+//true
+//false
+
+// != !==
+//opposite
