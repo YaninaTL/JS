963cc98ba32f221da4cb6afcff5c313f5cb69fb4
Yanina <moontmorency@gmail.com>
2022-09-22 10:52:25 +0200

    Learn functions, return, if, arrays, for loop

--- lesson 8/app.js
+++ lesson 8/app.js
@@ -22,3 +22,47 @@ for(let i = 0;i < names.length;i++) {
     // const fullName = `${names[i]} ${lastName}`
     //newArray.push(fullName);
     //Don't forget a console log (see above)
+
+    //Functions, return, if, arrays, for loop
+
+    const gas = [18, 20, 22, 23];
+    const food = [150, 60, 110];
+
+    function calculateTotal(arr) {
+      let total = 0;
+      for(let i = 0; i < arr.length,i++);
+      //i < arr.length -> it's a stopper
+      // {console.log(arr[i]);}
+      
+      {
+        total += arr[i]
+      }
+      // console.log(total)
+      //See the sum of: 18 + 20 + 22 + 23
+      
+      if(total > 100) {
+        console.log(`YOUR SPENDING ARE TOO HIGHT!`);
+        return total; 
+      }
+      console.log(`You are doing well!`);
+
+      return total;
+    }
+    // calculateTotal(gas);
+    //18, 20, 22, 23
+    //The function is working
+
+    const gasTotal = calculateTotal(gas);
+    const foodTotal = calculateTotal(food);
+    const randomTotal = calculateTotal([200, 4000, 500, 1]);
+
+    console.log({
+      gas:gasTotal,
+      food:foodTotal,
+      random:randomTotal,
+    });
+    //gas: sum, food: sum....
+
+
+
+
