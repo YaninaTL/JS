00475ed34e5f5375cf275f7fb0cef6b16bd2479c
Yanina <moontmorency@gmail.com>
2022-10-05 22:19:59 +0200

    Start learning callback

--- lesson 9/app.js
+++ lesson 9/app.js
@@ -2,43 +2,69 @@
 
 //Global -> access the value anywhere
 
-//Local 
+//Local
 
-let name = 'Bob';
+let name = "Bob";
 
 function calculate() {
-    //Going to add name
-    const name = 'John'
-    //Nothing has changed in console
+  //Going to add name
+  const name = "John";
+  //Nothing has changed in console
 
-    //Add a new one
-    const age = "25";
+  //Add a new one
+  const age = "25";
 
-    becomesGlobal = 'global variable';
-    //Can be accessed if called and logged as JS helps to identify the value (don't have key words: let/const)
-};
+  becomesGlobal = "global variable";
+  //Can be accessed if called and logged as JS helps to identify the value (don't have key words: let/const)
+}
 calculate();
 console.log(age);
 //Error -> We cannot access the value outside
 
 if (true) {
   //Going to add name
-    const name = "John";
+  const name = "John";
   //Nothing has changed in console
 }
 
 console.log(`My name is ${name}`);
 //...Bob
 
-
 //Variable Lookup
 const globalNumber = 5;
 
-function add(num1, num2){
-const result = num1 + num2 + globalNumber;
-return result;
+function add(num1, num2) {
+  const result = num1 + num2 + globalNumber;
+  return result;
 }
 
-console.log(add(3,4));
+console.log(add(3, 4));
 //values for num1, num2
 
+/* function greetMorning(aName) {
+  const myName = "Yanina";
+  console.log(`Good morning ${aName}, my name is ${myName}`);
+}
+
+greetMorning("Bob");
+*/
+
+//Good morning Bob, my name is Yanina
+
+//If we have a lot of options ->
+//Morning, afternoon, evening
+
+function morning() {
+  return `Good morning, Bob`;
+}
+
+function greet(aName, cb) {
+  const aName = "Yanina";
+  console.log(`${aName}, my name is ${myName}`);
+  cb();
+}
+
+greet("Bob", morning);
+greet("Peter", morning);
+
+//Callback
