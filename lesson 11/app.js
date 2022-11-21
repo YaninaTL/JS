590a6615e95c3549a526873c0eb8bd0e8b54c65f
Yanina <moontmorency@gmail.com>
2022-11-21 22:59:00 +0300

    learn Math

---
+++ lesson 11/app.js
@@ -0,0 +1,37 @@
+//Math
+
+const number1 = 4.56789;
+const result1 = Math.floor(number1);
+// 4
+
+const number2 = 4.56789;
+const result2 = Math.ceil(number2);
+//5
+
+const number3 = 4.56789;
+const result3 = Math.sqrt(number3);
+
+const number4 = 4.56789;
+const result4 = Math.ceil(number4);
+
+const result5 = Math.PI;
+
+const result6 = Math.min(5, 1, 6);
+//1
+
+const result7 = Math.max(5, 1, 6);
+//6
+
+const result8 = Math.random();
+//NEVER gives 1
+
+const result9 = Math.random() * 10;
+// Have bigger numbers
+
+const result10 = Math.floor(Math.random() * 10);
+//remove decimals
+//CANNOT give 10 or 1
+
+const result11 = Math.ceil(Math.random() * 10);
+// Give from 1 to 10
+//const result10 = Math.floor(Math.random() * 10 + 1);
