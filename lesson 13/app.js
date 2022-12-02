4c74136382c85593db8ce320bb0bcd005537f497
Yanina <moontmorency@gmail.com>
2022-12-02 20:23:56 +0300

    learn classes

--- lesson 13/app.js
+++ lesson 13/app.js
@@ -17,3 +17,38 @@ const showLink = link.getAttribute("href");
 const last = link.nextElementSibling;
 last.setAttribute("class", "first");
 last.textContent = "I also have a class of first";
+
+//Classes
+const first1 = document.getElementById("first");
+const second2 = document.getElementById("second");
+const third3 = document.getElementById("third");
+
+const classValue1 = first1.className;
+console.log(classValue1);
+//to check properties
+
+second2.className = "colors";
+//added
+
+second2.className = "text";
+//second2.className = "colors text";
+
+third3.classList.add("colors");
+third3.classList.add("text");
+//added
+//OR
+third3.classList.add("text", "colors");
+
+third3.classList.remove("text", "colors");
+//removed
+
+let result = third3.classList.contains("color");
+//has a class
+if (result) {
+  console.log("hello");
+} else {
+  console.log("bye");
+}
+
+const classValue3 = third3.classList;
+console.log(classValue3);
